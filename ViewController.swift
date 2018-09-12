12677b3b8c8e64b9c6bf10591d4624583013755a
Brian Hamm <hello@starsite.co>
2018-09-12 00:03:22 -0500

    Create ViewController.swift

---
+++ ViewController.swift
@@ -0,0 +1,131 @@
+import UIKit
+ 
+class ViewController: UIViewController {
+ 
+//  let baseURL = UserDefaults.standard.string(forKey: "fm-db-path")   // better
+//  let auth    = UserDefaults.standard.string(forKey: "fm-auth")      // better
+ 
+    let baseURL = URL(string: "https: //<hostName>/fmi/data/v1/databases/<databaseName>")!
+    let auth    = "xxxxxxxabcdefg1234567"  // base64 "user:pass"
+ 
+    var token   = UserDefaults.standard.string(forKey: "fm-token")
+    var expiry  = UserDefaults.standard.object(forKey: "fm-token-expiry") as? Date ?? Date(timeIntervalSince1970: 0)
+ 
+    // ... (cont'd)
+    
+
+
+
+    // active token?
+    func isActiveToken() -> Bool {
+        if let _ = self.token, self.expiry > Date() {
+            return true
+        } else {
+            return false
+        }
+    }
+
+
+
+
+    // refresh token
+    func refreshToken(for auth: String, completion: @escaping (String, Date) -> Void) {
+       
+        let url = baseURL.appendingPathComponent("/sessions")
+        let expiry = Date(timeIntervalSinceNow: 900)   // 15 minutes
+       
+        // request
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("Basic \(auth)", forHTTPHeaderField: "Authorization")
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+       
+        // task
+        URLSession.shared.dataTask(with: request) { data, _, error in
+           
+            guard   let data  = data, error == nil,
+                    let json  = try? JSONSerialization.jsonObject(with: data) as! [String: Any],
+                    let resp  = json["response"] as? [String: Any],
+                    let token = resp["token"] as? String
+            else {
+                    print("refresh token sad")
+                    return
+            }
+           
+            // prefs
+            self.token  = token
+            self.expiry = expiry
+           
+            completion(token, expiry)   // out^
+           
+        }.resume()
+    }
+    
+    
+    
+    
+    // query
+    var payload = ["query": [   // or ->[[p1],[p2]]   and ->[[p1,p2]]
+        ["bandName": "Daniel Markham"],
+        ["bandName": "Sudie"]
+    ]]
+ 
+ 
+    // data api find request
+    func findRequest(with token: String, layout: String, payload: [String: Any]) {
+       
+        let url = baseURL.appendingPathComponent("/layouts/\(layout)/_find")
+ 
+        // serialize             
+        guard let body = try? JSONSerialization.data(withJSONObject: payload) else { return }
+       
+        // request
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = body
+       
+        // task
+        URLSession.shared.dataTask(with: request) { data, _, error in
+           
+            guard   let data = data, error == nil,
+                    let json = try? JSONSerialization.jsonObject(with: data) as! [String: Any]
+            else {
+                    print("api request sad")
+                    return
+            }
+           
+            print("\n\(json)")   // disco!
+           
+        }.resume()
+    }
+
+
+
+
+
+    // did load
+    override func viewDidLoad() {
+        super.viewDidLoad()
+   
+        // apiRequest
+        switch isActiveToken() {   
+        case true:
+            print("with active token - expiry: \(self.expiry)")
+            findRequest(with: self.token!, layout: "Bands", payload: self.payload)
+ 
+        case false:
+            refreshToken(for: auth, completion: { newToken, newExpiry in    // async
+                print("fetch with new token - expiry: \(newExpiry)")
+                self.findRequest(with: newToken, layout: "Bands", payload: self.payload)
+            })
+        }
+        // ....
+ 
+    }  // .did load
+
+
+
+
+} // .view
