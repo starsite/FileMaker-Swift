109276ba06588d065bd1c11b02c155c6e5cbbe9f
Brian Hamm <hello@starsite.co>
2018-09-17 04:58:44 -0500

    Update Data-API-Service-Class.swift
    Mostly done, still a work in progress...

--- Data-API-Service-Class.swift
+++ Data-API-Service-Class.swift
@@ -1,3 +1,4 @@
+/////
 //
 //  DataAPI.swift
 //  Art Con
@@ -8,11 +9,11 @@
 
 import Foundation
 
-
 class DataAPI {
     
 //     let auth = UserDefaults.standard.string(forKey: "fm-auth")
 //     static let auth = "adjfkla;dfj..."
+    
     
     
     // active token?
@@ -27,7 +28,6 @@ class DataAPI {
             return false
         }
     }
-    
     
     
     
@@ -65,10 +65,10 @@ class DataAPI {
             
         }.resume()
     }
-
     
     
-        
+    
+    
     // get records -> ([records], error)
     class func getRecords(token: String, layout: String, limit: Int, completion: @escaping ([[String: Any]], String) -> Void) {
         
@@ -102,14 +102,55 @@ class DataAPI {
 
     
     
+    
+    // find request -> ([records], error)
+    class func findRequest(token: String, layout: String, payload: [String: Any], completion: @escaping ([[String: Any]], String) -> Void) {
+        
+        //  payload = ["query": [             payload = ["query": [
+        //    ["firstName": "Brian"],           "firstName": "Brian",
+        //    ["firstName": Geoff"]             "lastName": "Hamm"
+        //  ]]                                ]]
+        
+        guard   let path = UserDefaults.standard.string(forKey: "fm-db-path"),
+                let baseURL = URL(string: path),
+                let body = try? JSONSerialization.data(withJSONObject: payload) else { return }
+        
+        let url = baseURL.appendingPathComponent("/layouts/\(layout)/_find")
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = body
+        
+        URLSession.shared.dataTask(with: request) { data, _, error in
             
-    // get record by id -> (record, error)
-    class func getRecord(token: String, layout: String, recordId: Int, completion: @escaping ([String: Any], String) -> Void) {
+            guard   let data      = data, error == nil,
+                    let json      = try? JSONSerialization.jsonObject(with: data) as! [String: Any],
+                    let response  = json["response"] as? [String: Any],
+                    let messages  = json["messages"] as? [[String: Any]],
+                    let error     = messages[0]["code"] as? String else { return }
+            
+            guard let records = response["data"] as? [[String: Any]] else {
+                print(messages)
+                return
+            }
+            
+            completion(records, error)
+            
+        }.resume()
+    }
+    
+    
+    
+    
+    // get record with id -> (record, error)
+    class func getRecordWith(id: Int, token: String, layout: String, completion: @escaping ([String: Any], String) -> Void) {
         
         guard   let path = UserDefaults.standard.string(forKey: "fm-db-path"),
                 let baseURL = URL(string: path) else { return }
-
-        let url = baseURL.appendingPathComponent("/layouts/\(layout)/records/\(recordId)")
+        
+        let url = baseURL.appendingPathComponent("/layouts/\(layout)/records/\(id)")
         
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
@@ -133,58 +174,17 @@ class DataAPI {
             
         }.resume()
     }
-
-    
-    
-    
-    /// find request -> ([records], error)
-    class func findRequest(token: String, layout: String, payload: [String: Any], completion: @escaping ([[String: Any]], String) -> Void) {
-        
-        //  payload = ["query": [             payload = ["query": [
-        //      ["firstName": "Brian"],           "firstName": "Brian",
-        //      ["firstName": Geoff"]             "lastName": "Hamm"
-        //  ]]                                ]]
-        
-        guard   let path = UserDefaults.standard.string(forKey: "fm-db-path"),
-                let baseURL = URL(string: path),
-                let body = try? JSONSerialization.data(withJSONObject: payload) else { return }
-
-        let url = baseURL.appendingPathComponent("/layouts/\(layout)/_find")
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpBody = body
-        
-        URLSession.shared.dataTask(with: request) { data, _, error in
-            
-            guard   let data      = data, error == nil,
-                    let json      = try? JSONSerialization.jsonObject(with: data) as! [String: Any],
-                    let response  = json["response"] as? [String: Any],
-                    let messages  = json["messages"] as? [[String: Any]],
-                    let error     = messages[0]["code"] as? String else { return }
-            
-            guard let records = response["data"] as? [[String: Any]] else {
-                print(messages)
-                return
-            }
-        
-            completion(records, error)
-            
-        }.resume()
-    }
     
     
     
     
-    // delete record by id -> (error)
-    class func deleteRecord(token: String, layout: String, recordId: Int, completion: @escaping (String) -> Void) {
+    // delete record with id -> (error)
+    class func deleteRecordWith(id: Int, token: String, layout: String, completion: @escaping (String) -> Void) {
         
         guard   let path = UserDefaults.standard.string(forKey: "fm-db-path"),
                 let baseURL = URL(string: path) else { return }
-
-        let url = baseURL.appendingPathComponent("/layouts/\(layout)/records/\(recordId)")
+        
+        let url = baseURL.appendingPathComponent("/layouts/\(layout)/records/\(id)")
         
         var request = URLRequest(url: url)
         request.httpMethod = "DELETE"
@@ -204,26 +204,26 @@ class DataAPI {
             }
             
             completion(error)
-
+            
         }.resume()
     }
     
     
     
     
-    // edit record -> (error)
-    class func editRecord(token: String, layout: String, payload: [String: Any], recordId: Int, modID: Int?, completion: @escaping (String) -> Void) {
+    // edit record with id -> (error)
+    class func editRecordWith(id: Int, token: String, layout: String, payload: [String: Any], modID: Int?, completion: @escaping (String) -> Void) {
         
         //  payload = ["fieldData": [
-        //      "firstName": "newValue",
-        //      "lastName": newValue"
+        //    "firstName": "newValue",
+        //    "lastName": newValue"
         //  ]]
         
         guard   let path = UserDefaults.standard.string(forKey: "fm-db-path"),
                 let baseURL = URL(string: path),
                 let body = try? JSONSerialization.data(withJSONObject: payload) else { return }
-
-        let url = baseURL.appendingPathComponent("/layouts/\(layout)/records/\(recordId)")
+        
+        let url = baseURL.appendingPathComponent("/layouts/\(layout)/records/\(id)")
         
         var request = URLRequest(url: url)
         request.httpMethod = "PATCH"
@@ -242,7 +242,7 @@ class DataAPI {
                 print(messages)
                 return
             }
-
+            
             completion(error)
             
         }.resume()
@@ -252,17 +252,17 @@ class DataAPI {
     
     
     
-/*
+    /*
      
-400     Bad request             Occurs when the server cannot process the request due to a client error.
-401     Unauthorized            Occurs when the client is not authorized to access the API. If this error occurs when attempting to log in to a database session, then there is a problem with the specified user account or password. If this error occurs with other calls, the access token is not specified or it is not valid.
-403     Forbidden               Occurs when the client is authorized, but the call attempts an action that is forbidden for a different reason.
-404     Not found               Occurs if the call uses a URL with an invalid URL schema. Check the specified URL for syntax errors.
-405     Method not allowed      Occurs when an incorrect HTTP method is used with a call.
-415     Unsupported media type  Occurs if the required header is missing or is not correct for the request: For requests that require "Content-Type: application/json" header, occurs if the "Content-Type: application/json" header is not specified or if a different content type was specified instead of the "application/json" type. For requests that require "Content-Type: multipart/form-data" header, occurs if the "Content-Type: multipart/form-data" header is not specified or if a different content type was specified instead of the "multipart/form-data" type.
-500     FileMaker Server error  Includes FileMaker error messages and error codes. See FileMaker error codes in FileMaker Pro Advanced Help.
+     400     Bad request             Occurs when the server cannot process the request due to a client error.
+     401     Unauthorized            Occurs when the client is not authorized to access the API. If this error occurs when attempting to log in to a database session, then there is a problem with the specified user account or password. If this error occurs with other calls, the access token is not specified or it is not valid.
+     403     Forbidden               Occurs when the client is authorized, but the call attempts an action that is forbidden for a different reason.
+     404     Not found               Occurs if the call uses a URL with an invalid URL schema. Check the specified URL for syntax errors.
+     405     Method not allowed      Occurs when an incorrect HTTP method is used with a call.
+     415     Unsupported media type  Occurs if the required header is missing or is not correct for the request: For requests that require "Content-Type: application/json" header, occurs if the "Content-Type: application/json" header is not specified or if a different content type was specified instead of the "application/json" type. For requests that require "Content-Type: multipart/form-data" header, occurs if the "Content-Type: multipart/form-data" header is not specified or if a different content type was specified instead of the "multipart/form-data" type.
+     500     FileMaker Server error  Includes FileMaker error messages and error codes. See FileMaker error codes in FileMaker Pro Advanced Help.
      
-*/
+     */
     
     
 }
