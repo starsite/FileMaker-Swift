da213a347d1dafdef9b3461c712c59489128747a
Brian Hamm <hello@starsite.co>
2018-10-16 09:19:58 -0500

    Update DataAPI.swift

--- DataAPI.swift
+++ DataAPI.swift
@@ -10,21 +10,21 @@ import Foundation
 
 
 class DataAPI {    
+        
+//  let baseURL = "https://<hostName>/fmi/data/v1/databases/<databaseName>"
+//  let auth    = "xxxxxabcdefg1234567"
     
-    let auth = UserDefaults.standard.string(forKey: "fm-auth")      // fetch from CloudKit at launch, or...
-//  let auth = "xxxxxabcdefg1234567"
+    let baseURL = UserDefaults.standard.string(forKey: "fm-db-path")   // better    
+    let auth    = UserDefaults.standard.string(forKey: "fm-auth")      // better
     
-    let path = UserDefaults.standard.string(forKey: "fm-db-path")   // fetch from CloudKit at launch, or...
-//  let path = "https://<hostName>/fmi/data/v1/databases/<databaseName>"
-    
+    let token   = UserDefaults.standard.string(forKey: "fm-token")
+    let expiry  = UserDefaults.standard.object(forKey: "fm-token-expiry") as? Date ?? Date(timeIntervalSince1970: 0)
+
     
     
     // active token?
     class func isActiveToken() -> Bool {
-        
-        let token   = UserDefaults.standard.string(forKey: "fm-token")
-        let expiry  = UserDefaults.standard.object(forKey: "fm-token-expiry") as? Date ?? Date(timeIntervalSince1970: 0)
-        
+                
         if let _ = token, expiry > Date() {
             return true
         } else {
@@ -110,8 +110,8 @@ class DataAPI {
     class func findRequest(token: String, layout: String, payload: [String: Any], completion: @escaping ([[String: Any]], String) -> Void) {
         
         //  payload = ["query": [             payload = ["query": [
-        //      ["firstName": "Brian"],           "firstName": "Brian",
-        //      ["firstName": Geoff"]             "lastName": "Hamm"
+        //    ["firstName": "Brian"],           "firstName": "Brian",
+        //    ["firstName": Geoff"]             "lastName": "Hamm"
         //  ]]                                ]]
         
         guard   let path = UserDefaults.standard.string(forKey: "fm-db-path"),
