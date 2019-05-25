d7bc9d7e030b6ced52747766091c5e9b6bb589d7
Brian Hamm <hello@starsite.co>
2019-05-24 21:18:59 -0500

    Update DataAPI.swift

--- DataAPI.swift
+++ DataAPI.swift
@@ -70,9 +70,47 @@ class DataAPI {
         }.resume()
     }
     
+         
+         
+         
     
-    
-    
+    // returns -> (error code)
+    class func createRecord(token: String, layout: String, payload: [String: Any], completion: @escaping (String) -> Void ) {
+             
+        //  payload = ["fieldData": [
+        //      ["firstName": "Brian"],
+        //      ["lastName": "Hamm"],
+        //      ["age": 47]
+        //  ]]
+
+        guard   let path = UserDefaults.standard.string(forKey: "fm-db-path"),
+                let baseURL = URL(string: path),
+                let body = try? JSONSerialization.data(withJSONObject: payload) else { return }
+
+        let url = baseURL.appendingPathComponent("/layouts/\(layout)/records")
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = body
+
+        URLSession.shared.dataTask(with: request) { data, _, error in
+            
+            guard   let data      = data, error == nil,
+                    let json      = try? JSONSerialization.jsonObject(with: data) as! [String: Any],
+                    let messages  = json["messages"] as? [[String: Any]],
+                    let code      = messages[0]["code"] as? String else { return }
+                        
+            completion(code)
+            
+        }.resume()
+    }
+  
+         
+         
+         
+         
     // returns -> ([records], error code)
     class func getRecords(token: String, layout: String, offset: Int, limit: Int, completion: @escaping ([[String: Any]], String) -> Void) {
         
